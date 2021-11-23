37a993980ff1bea390c1d463ab1876e45cbf7e56
WEEKEND88 <sergutin.03@mail.ru>
2021-11-23 17:00:44 +0200

    Second Commit

---
+++ local/templates/inner/description.php
@@ -0,0 +1,8 @@
+<?
+$arTemplate = array (
+  'NAME' => 'Внутренние',
+  'DESCRIPTION' => 'внутр шаблон',
+  'SORT' => '',
+  'TYPE' => '',
+);
+?>
---
+++ local/templates/inner/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta class="js-meta-viewport" name="viewport" content="width=device-width, height=device-height, initial-scale=1, shrink-to-fit=no, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+    <meta name="format-detection" content="telephone=no">
+    <title>Yanicode :: Главная</title>
+    <link rel="stylesheet" href="/local/templates/inner/assets/template_styles.css">
+</head>
+<body>
+<header class="header">
+    <div class="container">
+        <div class="header-wrapper">
+            <a href="./index.html" class="header__logo">
+                <img width="300" height="92" src="/local/templates/inner/assets/images/svg/logo-yanicode.svg" alt="yanicode">
+            </a>
+            <div class="header__burger header__burger_close">
+                <span class="burger-line"></span>
+                <span class="burger-line"></span>
+                <span class="burger-line"></span>
+            </div>
+            <div class="header-nav">
+                <nav class="nav-list">
+                    <a href="./index.html" class="nav-list__item">ГЛАВНАЯ</a>
+                    <a href="./services.html" class="nav-list__item">УСЛУГИ</a>
+                </nav>
+                <div class="header__phone">
+                    <a href="tel:+79114510616">+79114510616</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
